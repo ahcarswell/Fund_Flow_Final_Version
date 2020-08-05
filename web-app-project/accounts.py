4c02a2a5a4f362d43236ec035f099659a5409cd4
Andrea Llamas <andreaillamas@yahoo.com>
2020-08-04 20:30:17 -0400

    update back_end files for integration

--- web-app-project/accounts.py
+++ web-app-project/accounts.py
@@ -36,37 +36,26 @@ def create_account():
     # This gets the data sent over from an ajax call (if that's what's being used)
     data = request.get_json()
     user_list = get_user_list()
-    email_list = get_email_list()
+    #email_list = # get_email_list()
 
     # Dictionary values aren't set in stone. Need to change it to what values are sent over from frontend
-    if data["password"] != data["confirm_password"]:
-        response = {
-            "error": "The passwords do not match.",
-            "success": False
-        }
+    if data["password"] != data["passConf"]:
+
         # Could be render template depending on the front end
-        return jsonify(response=response)
+        return {"username": data["username"], "pass": data["pass"], "verified": False}
     # The username is already present in the DB
     elif data["username"] in user_list:
-        response = {
-            "error": "The username is already in use. Please choose a new one.",
-            "success": False
-        }
+  
         # Could be render template depending on the front end
-        return jsonify(response=response)
+        return {"username": data["username"], "pass": data["pass"], "verified": False}
 
     elif data["email"] in email_list:
-        response = {
-            "error": "The email is already in use. Please enter a new one.",
-            "success": False
-        }
-        return jsonify(response=response)
+        return {"username": data["username"], "pass": data["pass"], "verified": False}
     else:
         data = {
             'name': data["name"],
             'username': data["username"],
-            'password': data["password"],
-            'email': data["email"]
+            'password': data["pass"],
         }
         res = firebase.post('/fundflow-team3/Users', data)
 
@@ -80,7 +69,7 @@ def create_account():
         donations_result = firebase.post('/fundflow-team3/past_donations', donations_set_up)
         # Based on a route to a dashboard that is specific to the user
        # return render_template("dashboard.html", user_info=data["username"])
-        return jsonify(data=data)
+        return {"username": data["username"], "pass": data["pass"], "verified": True}
 
 
 @accounts.route('/backend/login', methods=['GET', 'POST'])
@@ -91,27 +80,19 @@ def login():
     user_list = get_user_list()
 
     if data["username"] not in user_list:
-        response = {
-            "error": "We do not have an account registered under that username",
-            "success": False
-        }
-        return jsonify(response=response)
+        return {"username": data["username"], "pass": data["pass"], "verified": False}
 
     for k, v in result.items():
         if isinstance(v, dict) and v["username"] == data["username"]:
-            if v["password"] == data["password"]:
+            if v["password"] == data["pass"]:
                 data = {"message" : "login successful"}
-                return jsonify(data=data)
+                return {"username": data["username"], "pass": data["pass"], "verified": True}
                 #elif data["password"] == full_dict[data["username"]]:
 
         #return render_template("dashboard.html", user=data["username"])
         # Does full_dict check everything at once? #full dict gets all the users in our database
     else:
-        response = {
-            "error": "The password does not match the password in the system",
-            "success": False
-        }
-        return jsonify(response=response)
+        return {"username": data["username"], "pass": data["pass"], "verified": False}
 
 
 # test create_account("Andrea", "andi", "andi101", "andi101", "andrea@gmail.com")
